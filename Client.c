93d9f88a4d5a4637bce8e790df28df1765d76c46
João Batista <jnfbatista@gmail.com>
2009-10-11 18:52:58 +0100

    Some progress done

--- Client.c
+++ Client.c
@@ -1,11 +1,30 @@
 #include <stdio.h>
 #include <netinet/in.h>
+#include <sys/socket.h>
+
 
 int main( int argc, char * argv[]) {
+	int i = 0, socket_fd;
 
-}
+	//socket address
+	struct sockaddr_in sock_addr;
 
-int initiateSocket( int port) {
 
+	if ( argc < 5) {
+		printf("wrong input");
+	} else {
+		int socket_fd;
+
+		if (	socket_fd = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP) == -1) {
+			perror("socket");
+			return -1;	
+		} 
+
+		sock_addr.sin_family = AF_INET;
+		sock_addr.sin_port = htons(argv[2]);
+		sock_addr.sin_addr = 
+
+	}
+	return 0;
 }
 
--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
 server:
 	javac Server.java
 
+edit:
+	vim -p Server.java Client.c
+
 clean:
 	rm *.class
